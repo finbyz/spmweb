048165f9eabf3e41c5fe9c266ddef20256f2a51c
Ubuntu <ubuntu@ip-172-31-22-80.ap-south-1.compute.internal>
2022-08-12 09:52:24 +0000

    item related changes

--- spmweb/www/items.py
+++ spmweb/www/items.py
@@ -5,6 +5,12 @@ no_cache = 1
 no_sitemap = 1
 
 def get_context(context):
-	data = frappe.get_all('Item', fields=['name', 'item_name','item_group', 'description', 'image','route'],
-			filters={'show_in_website': 1})
+	data=frappe.db.sql("""
+	select item.item_code,item.item_name,item.item_group,wi.description,item.image,wi.route,wi.website_image
+	from `tabItem` as item
+	join `tabWebsite Item` as wi where wi.item_code=item.item_code and item.show_in_website = 1
+	""",as_dict=1)
+	# data = frappe.get_all('Website Item', fields=['item_code', 'item_name','item_group', 'description', 'image','route'],
+	# 		filters={'published': 1})
+
 	return {'doc':data}
